ac780c6e948fc3c6b43851f02dbe3ca7d87720e4
hyun1e <jasmine.kim.18@gmail.com>
2017-12-07 14:05:16 -0800

    Added useful comment

--- find_patterns.py
+++ find_patterns.py
@@ -3,8 +3,10 @@ import star_averager
 
 
 ############################################
-gen_category = "healthy"
-categories = ["Acai Bowls", "Farmers Market", "Honey", "Poke", "Kombucha"]
+gen_category = "restaurants"
+categories = ["Restaurants"]
+## NOTE:
+# First time, you have to run category.py to get the categories.
 
 get_csv.create_csv(gen_category, categories)
 star_averager.star_plotter(gen_category)
