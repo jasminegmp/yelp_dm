4afd9e18bb1f320fc1ed9ce2473ece88769a051a
Jasmine Kim <jasmine.kim.18@gmail.com>
2017-12-10 21:38:46 -0800

    Made one minor change to fix mistake in code

--- final_code/test_script.py
+++ final_code/test_script.py
@@ -14,7 +14,7 @@ print ""
 print "Performing data pre-processing..." 
 
 # Set this flag if this if the first time you're running through the script
-first_time_flag = 1
+first_time_flag = 0
 
 if first_time_flag:
 	print ""
@@ -34,7 +34,7 @@ test_lib.run_test(1, "restaurants_category", 0)
 
 # Second test runs comparison for all restaurant categories against each other
 # Uses rolling window mean, DTW, time normalization using quartiles, and Z normalization
-test_lib.run_test(2, "restaurants_category", 0, 0)
+test_lib.run_test(2, "restaurants_category", 0)
 
 # Third test runs comparison for Chipotle against all other restaurant categories
 test_lib.run_test(3, "Chipotle Mexican Grill", "restaurants_category")
@@ -43,4 +43,4 @@ test_lib.run_test(3, "Chipotle Mexican Grill", "restaurants_category")
 test_lib.run_test(4, "Chipotle Mexican Grill", "Mexican")
 
 # Plot for Chipotle
-review_plotter.review_plotter("Chipotle Mexican Grill")
+#review_plotter.review_plotter("Chipotle Mexican Grill")
